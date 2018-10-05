49b81e29b6f966bea29529adbd0d688f8a1cd590
Noelle Rolwes <nrolwes@gmail.com>
2018-10-05 12:29:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the #alan alans_new_info =
-programmer_hash[:alan_kay][:known_for] = {alans_new_info: "GUI"}
+programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash[:alan_kay][:known_for]  
  
  
